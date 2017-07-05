d4ddaec444d20b016154e2922dff9d6a7a4a59f3
quallsma <quallsma@msu.edu>
2017-07-05 09:37:53 -0400

    Some more refactoring

--- src/main/java/PrimeFactor/PrimeFactorGenerator.java
+++ src/main/java/PrimeFactor/PrimeFactorGenerator.java
@@ -2,30 +2,19 @@ package PrimeFactor;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PrimeFactorGenerator {
 
     public boolean IsPrime(int number){
-        int halfOfNumber = number / 2;
-        int index = 2;
-        while(index <= halfOfNumber){
-            if (number % index == 0)
+        for(int index = 2; index <= number / 2; index++){
+            if(number % index == 0)
                 return false;
-            index++;
         }
-
         return true;
     }
 
     public List<Integer> GetPrimeFactors(int number){
-
-        if (number <= 1)
-            return Arrays.asList();
-
-        if (IsPrime(number))
-            return Arrays.asList(number);
 
         List<Integer> list = new ArrayList<>();
         for (int x = 2;number >= 2; x++){
